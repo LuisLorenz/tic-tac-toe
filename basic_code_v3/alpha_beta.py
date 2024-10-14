b7ffce59866c1f4b716b598d5792e321c9e968cf
Luis Lorenzs <luislorenz7@gmail.com>
2024-10-14 20:44:36 +0200

    working on AI player

---
+++ basic_code_v3/alpha_beta.py
@@ -0,0 +1,4 @@
+# https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/
+# making the searching algorithm more efficient by avoid searching game trees that are obiviousely not the best move 
+    # because a better move was found 
+# alpha-beta 
