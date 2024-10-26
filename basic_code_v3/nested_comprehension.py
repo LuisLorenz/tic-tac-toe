3f3f91b84e1eba5efc240cc74fde903ff2808bf7
Luis Lorenzs <luislorenz7@gmail.com>
2024-10-26 10:15:50 +0200

    set and dic

---
+++ basic_code_v3/nested_comprehension.py
@@ -0,0 +1,11 @@
+cities = ["Austin", "Tacoma", "Topeka", "Sacramento", "Charlotte"]
+var = {city: [0 for _ in range(7)] for city in cities}
+print(var)
+    # {'Austin': [0, 0, 0, 0, 0, 0, 0], 
+    # 'Tacoma': [0, 0, 0, 0, 0, 0, 0], 
+    # 'Topeka': [0, 0, 0, 0, 0, 0, 0], 
+    # 'Sacramento': [0, 0, 0, 0, 0, 0, 0], 
+    # 'Charlotte': [0, 0, 0, 0, 0, 0, 0]}
+
+    # city names are invidual keys 
+    
---
+++ basic_code_v3/set_dic_list_comp.py
@@ -0,0 +1,26 @@
+# https://realpython.com/list-comprehension-python/
+
+# set comprehension
+quote = "life, uh, finds a way"
+vowels = {char for char in quote if char in "aeiou"}
+print(vowels)
+    # {'e', 'u', 'a', 'i'}
+    # {'i', 'a', 'e', 'u'}
+    # {'a', 'e', 'u', 'i'}
+
+    # no particular order
+    # no doubled elements
+
+# dictionary comprehension
+var = {number: number * number for number in range(10)}
+print(var)
+    # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
+
+# walrus operator
+import random
+def get_weather_data():
+   return random.randrange(90, 110)
+
+
+var = [temp for _ in range(20) if (temp := get_weather_data()) >= 100]
+print(var)
