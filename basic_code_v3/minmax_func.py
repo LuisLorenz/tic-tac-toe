a2f1d33a5d115161b9100ac0dc52e96c2dcaa369
Luis Lorenzs <luislorenz7@gmail.com>
2024-10-28 19:36:38 +0100

    generator function

--- basic_code_v3/minmax_func.py
+++ basic_code_v3/minmax_func.py
@@ -1,2 +1,4 @@
 # https://realpython.com/python-min-and-max/
 # finding the smallest and largest value in an iterable 
+
+# continue here: Getting Started With Python’s min() and max() Functions
--- basic_code_v3/set_dic_list_comp.py
+++ basic_code_v3/set_dic_list_comp.py
@@ -27,3 +27,49 @@ print(var)
     # [100, 109, 100, 107, 103, 104, 107, 108, 105, 102, 105, 100, 101, 106]
 
     # the temp var is created and called 
+
+# building the sum with a list comprehension
+value = sum([number * number for number in range(1000)])
+print(value) 
+# 332833500
+
+# calculating with large numbers
+# here a list comprehension is a bad choice
+    # each individual value is stored in a list which results as in huge list and take a lot of capacity
+# for this reason switching to a generator is the better choice
+    # one value gets updated and stored at a time 
+# value = sum(number * number for number in range(1_000_000_000))
+# print(value)
+# 333333332833333333500000000
+
+# alt with map()
+# sum(map(lambda number: number * number, range(1_000_000_000)))
+
+# timing 
+import random
+import timeit
+TAX_RATE = .08
+PRICES = [random.randrange(100) for _ in range(100_000)]
+def get_price(price):
+    return price * (1 + TAX_RATE)
+
+def get_prices_with_map():
+    return list(map(get_price, PRICES))
+
+def get_prices_with_comprehension():
+    return [get_price(price) for price in PRICES]
+
+def get_prices_with_loop():
+    prices = []
+    for price in PRICES:
+        prices.append(get_price(price))
+    return prices
+
+
+print(timeit.timeit(get_prices_with_map, number=100))
+
+
+print(timeit.timeit(get_prices_with_comprehension, number=100))
+
+
+print(timeit.timeit(get_prices_with_loop, number=100))
